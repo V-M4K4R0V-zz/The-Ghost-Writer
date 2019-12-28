e153ccb40bbe53d99004d50b053d98f7e1ff6c67
V-M4K4R0V <ahmedmontassir0@outlook.com>
2019-12-28 16:16:22 +0100

    arabic-baliiiz

---
+++ The-Poetry-Writer/the-poetry-writer.py
@@ -0,0 +1,17 @@
+#author : V-M4K4R0V
+
+print("choose your language || اختر لغتك")
+L_lang = input("[ar] or [eng] : ")
+L_name = input("YOUR LOVER NAME : ")
+L_gender = input("Your lover Gender [M]or[F] : ")
+if L_gender == 'M':
+    L_feel = input("do u [L]ove or [H]ate him [H]or[L] : ")
+    if L_feel == 'L':
+
+    elif == 'H':
+elif == 'F':
+    L_feel2 = input("do u [L]ove or [H]ate her [H]or[L] : ")
+    if L_feel2 == 'L':
+
+    elif == 'H':
+    
