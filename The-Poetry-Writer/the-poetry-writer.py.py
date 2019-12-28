5924a417beaf8f3f1b7472ce6407bf98b773d460
V-M4K4R0V <ahmedmontassir0@outlook.com>
2019-12-28 14:27:18 +0100

    HOW-TO-FUCKING-PRINT-IN-ARABIC

---
+++ The-Poetry-Writer/the-poetry-writer.py.py
@@ -0,0 +1,11 @@
+#author : V-M4K4R0V
+
+arab_1 =  "اختر لغتك"
+print("choose your language || " + arab_1)
+L_lang = input("[ar] or [eng] : ")
+L_name = input("YOUR LOVER NAME : ")
+L_gender = input("Your lover Gender [M]or[F] : ")
+if L_gender == 'M':
+    print("hi ahmed")
+else:
+    print("9o9o")
